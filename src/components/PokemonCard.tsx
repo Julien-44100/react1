fadc85d19517549f6093cd7935923345c8223f25
Julien-44100 <julienbotar44100@yahoo.com>
2024-10-23 10:58:46 +0200

    "change"

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -10,4 +10,15 @@ function Pokemoncard() {
   );
 }
 
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
+
 export default Pokemoncard;
